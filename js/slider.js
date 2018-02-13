a22172f5ca6acda1247459200575c5ff1dd5ab92
omayahu <omayahu@gmail.com>
2018-02-13 20:59:44 +0300

    ver 0.01

---
+++ js/slider.js
@@ -0,0 +1,61 @@
+var curSlide = 1;
+var slideCount = 0;
+var slideHeight = $(".slider").height();
+var interval = $(".slider").attr("slide-interval");
+var timerId;
+
+timerId = setInterval(next, interval);
+
+$(document).ready(function() {
+    $(".slider .slide").each(function() {
+        slideCount = slideCount + 1;
+        var src = $(this).children(".slide-img").attr("img-src");
+        $(this).children(".slide-img").css("background", "url(" + src + ") no-repeat center top");
+        $(this).children(".slide-img").css("background-size", "cover");
+        $(this).attr("id", "slide-" + slideCount.toString());
+        $(this).children(".slide-text").css("padding-top", (slideHeight/2-$(this).children(".slide-text").height().toString()));
+    });
+
+    $(".slide-pre").css("padding-top", (slideHeight/2-80).toString() + "px");
+    $(".slide-next").css("padding-top", (slideHeight/2-80).toString() + "px");
+
+    $(".slide-pre").html("<span class=\"icon-chevron-left\"></span>&#160;");
+    $(".slide-next").html("&#160;<span class=\"icon-chevron-right\"></span>");
+});
+
+function next() {
+    $("#slide-" + curSlide.toString()).removeClass("active");
+    if (curSlide < slideCount) {
+        curSlide = curSlide + 1;
+    }
+    else {
+        curSlide = 1;
+    }
+    $("#slide-" + curSlide.toString()).addClass("active");
+}
+
+function prev() {
+    $("#slide-" + curSlide.toString()).removeClass("active");
+    if (curSlide > 1) {
+        curSlide = curSlide - 1;
+    }
+    else {
+        curSlide = slideCount;
+    }
+    $("#slide-" + curSlide.toString()).addClass("active");
+}
+
+$(".slide-next").click(function() {
+    next();
+});   
+
+$(".slide-pre").click(function() {
+    prev();
+});
+
+$(".slider").hover(function() {
+    clearInterval(timerId);
+}, function() {
+    timerId = setInterval(next, interval);
+});
+
